888b7f5b65bf06dae1355bf8c6e5aec5e91a0935
MartinRudzki <martin.rudzki@yahoo.com>
2015-11-10 12:11:28 -0500

    yes

--- js/javascript.js
+++ js/javascript.js
@@ -106,7 +106,7 @@ Created: MR 10/20/2015
 $('#submmit').click(function () {
 */
 
-$(document).ready(function () {
+$('#submmit').click(function () {
    $('#form').validate({
     rules: {
         row: {
