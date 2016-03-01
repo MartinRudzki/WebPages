456b495f8aba3df725d9222983a7a8fc91a5d225
Dilanlius <dylan_wetherald@student.uml.edu>
2016-03-01 13:35:33 -0500

    Update from class 3/1

--- Sudoku/db.js
+++ Sudoku/db.js
@@ -1,57 +1,19 @@
 /* 
- * SudokuZenGarden
- * db.js
- Found a lot of this code at http://mongoosejs.com/docs/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 
 
 
-var mongoose = require('mongoose');
-var userSchema = new mongoose.Schema({
-    Username: String,
-    Password: String
+var mongoose = require('mongoose');  
+var userSchema = new mongoose.Schema({  
+  Username: String,
+  Password: String
 });
-
-// Connect to the db
-mongoose.connect("mongodb://localhost/Sudoku-Zen-Garden", function (err, db) {
-    if (err) {
-        return console.dir(err);
-    }
-    var db = mongoose.connection;
-    var collection = db.collection('User');
-
-});
-
-
-
-
-
-
-var User = mongoose.model('User', userSchema);
-
-var addUser = function (username, password) {
-    var temp = new User({Username: username, Password: password});
-    temp.save(function (err, User) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('saved successfully:', User);
-        }
-    });
-    return temp;
-};
-
-var loginValidation = function (username, password) {
-    var error = 'error';
-    var query = User.find({Username: username});
-    if (query.Password == password) {
-        return query;
-    }
-    else {
-        return error;
-    }
-};
+mongoose.connect('mongodb://localhost/Sudoku-Zen-Garden');
+mongoose.model('User', userSchema);  
 
 
 var user1 = new User({Username: 'Test', Password: 'password'});
@@ -59,10 +21,10 @@ var user1 = new User({Username: 'Test', Password: 'password'});
 user1.Username = user1.name.toUpperCase();//Test code to change username to uppercase
 console.log(user1);
 
-user1.save(function (err, User) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('saved successfully:', User);
-    }
+user1.save(function (err, userObj) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('saved successfully:', userObj);
+  }
 });
