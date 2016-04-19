082f37051dc85b60d272884db4a2335d383a63da
MartinRudzki <martin.rudzki@yahoo.com>
2016-04-19 12:53:36 -0400

    Update Other.js

--- Sudoku/java/Other.js
+++ Sudoku/java/Other.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
         var value = slider.slider('value'),
           volume = $('.volume');
         if (value <= 1) {
-          volume.css('background-position', '0 0');
+          volume.css('background-position', '0 0px');
         }
         else if (value <= 50) {
           volume.css('background-position', '0 -44px');
