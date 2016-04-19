7b22178656daeb2fbeffa00ae45422bc8634acce
MartinRudzki <martin.rudzki@yahoo.com>
2016-04-19 12:52:03 -0400

    Update Other.js

--- Sudoku/java/Other.js
+++ Sudoku/java/Other.js
@@ -30,8 +30,8 @@ $(document).ready(function () {
         // Volume meter transition
         var value = slider.slider('value'),
           volume = $('.volume');
-        if (value < 1) {
-          volume.css('background-position', '0 0px');
+        if (value <= 1) {
+          volume.css('background-position', '0 0');
         }
         else if (value <= 50) {
           volume.css('background-position', '0 -44px');
